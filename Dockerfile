207150772a3356b498fedd7bb00624130ae3697b
Lars Kellogg-Stedman <lars@redhat.com>
2015-04-08 12:31:58 -0400

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM fedora
+MAINTAINER Lars Kellogg-Stedman <lars@oddbit.com>
+EXPOSE 22
+
+RUN yum -y install \
+	    openssh-server \
+	    passwd \
+	    rdiff-backup \
+	    ; yum clean all
+
+COPY entrypoint.sh /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
+CMD ["/usr/sbin/sshd", "-D"]
+
---
+++ entrypoint.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+: ${SSH_USERNAME:=backup}
+: ${SSH_USERPASS:=$(dd if=/dev/urandom bs=1 count=15 | base64)}
+
+__create_rundir() {
+	mkdir -p /var/run/sshd
+}
+
+__create_default_user() {
+# Create a user to SSH into as.
+useradd $SSH_USERNAME
+echo -e "$SSH_USERPASS\n$SSH_USERPASS" | (passwd --stdin $SSH_USERNAME)
+echo ssh user password: $SSH_USERPASS
+}
+
+__create_other_users() {
+	set -x
+	USERS=/run/metadata/users
+	if [ -f $USERS ]; then
+		while read username userpass; do
+			[ "$username" ] || continue
+			useradd $username
+			if [ "$userpass" ] && [ "$userpass" != "-" ]; then
+				echo -e "$SSH_USERPASS\n$SSH_USERPASS" |
+					passwd --stdin $SSH_USERNAME
+			fi
+
+			userhome=$(getent passwd $username | cut -d: -f6)
+			install -m 700 -o $username -g $username \
+				-d "$userhome/.ssh"
+
+			if [ -f /run/metadata/$username.key ]; then
+				install -m 600 -o $username -g $username \
+					/run/metadata/$username.key \
+					"$userhome/.ssh/authorized_keys"
+			fi
+		done < $USERS
+	fi
+}
+
+__create_hostkeys() {
+ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -N '' 
+}
+
+# Call all functions
+__create_rundir
+__create_hostkeys
+__create_default_user
+__create_other_users
+
+exec "$@"
+
